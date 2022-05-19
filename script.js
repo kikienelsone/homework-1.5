c3f4225953e8b023cc0f678e3b9996e4ae529bd1
kikienelsone <81746445+kikienelsone@users.noreply.github.com>
2022-05-19 15:15:21 +0300

    fix slider

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
 
-    // If we need pagination
+    let swiper = new Swiper(".swiper", {
+    slidesPerView: 1.2,
+    spaceBetween: 30,
+    keyboard: {
+    enabled: true,
+},
     pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
+    el: ".swiper-pagination",
+    clickable: true,
+},
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+},
 });
+
+
+let activeBtn = document.querySelector(".brands_show-button")
+let hideBtn = document.querySelector(".brands_hide-button")
+let brands = document.querySelectorAll(".hidden")
+
+
+showBrands = () =>{
+    activeBtn.classList.add("hide")
+
+    for (let i = 0; i < brands.length; i++) {
+         brands[i].classList.add("active")
+     }
+    hideBtn.classList.add("active")
+}
+
+hideBrands = () =>{
+    hideBtn.classList.remove("active")
+    activeBtn.classList.add("active")
+    for (let i = 0; i < brands.length; i++) {
+         brands[i].classList.add("brandsHide")
+     }
+}
